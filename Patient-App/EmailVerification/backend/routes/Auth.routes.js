be1eff108a4c1c3beeb44d063950fae3ff9797b2
ManthanDeshmukh06 <manthandeshmukh06@gmail.com>
2025-10-02 00:46:26 +0530

    Initial commit

--- Patient-App/EmailVerification/backend/routes/Auth.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { register, VerifyEmail } from "../controllers/Auth.js";
-const AuthRoutes = express.Router();
-
-AuthRoutes.post("/register", register);
-AuthRoutes.post("/verify-email", VerifyEmail);
-export default AuthRoutes;
